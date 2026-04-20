e14fe9d3df92555eabc6ac5ed397a23d3cf58fa2
artemboiko42-boop <artem.boiko.42@gmail.com>
2026-04-20 09:45:51 +0300

    Delete public directory

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push',e=>{
- const d=e.data.json();
- e.waitUntil(self.registration.showNotification(d.title,{body:d.body,data:{url:d.url}}));
-});
-
-self.addEventListener('notificationclick',e=>{
- e.notification.close();
- e.waitUntil(clients.openWindow(e.notification.data.url));
-});

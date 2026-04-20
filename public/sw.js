abfb2dac3b86c5676b557b9ab48cc5b550cb7f75
artemboiko42-boop <artem.boiko.42@gmail.com>
2026-04-20 10:04:00 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('push',e=>{const d=e.data.json();e.waitUntil(self.registration.showNotification(d.title,{body:d.body,data:{url:d.url}}))});self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow(e.notification.data.url))});
